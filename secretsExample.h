94f4f23bbfdd5e21754da55152997de32be6ee72
alexander.fitzgibbon@yahoo.de <145123920+Gibbonator-Alex@users.noreply.github.com>
2025-10-01 23:59:04 +0200

    Added Topic to secretsExample

--- secretsExample.h
+++ secretsExample.h
@@ -18,12 +18,14 @@ const char *TOPIC_LIGHT_INTENSITY   = "";
 const char *TOPIC_DELAY_BETWEEN_PIX = "";
 const char *TOPIC_LED_ON_DURATION   = "";
 const char *TOPIC_LED_COLOR         = "";
+const char *TOPIC_LED_ENABLED       = "";
 
 const char* TOPICS[] = {
   TOPIC_LIGHT_INTENSITY,
   TOPIC_DELAY_BETWEEN_PIX,
   TOPIC_LED_ON_DURATION,
-  TOPIC_LED_COLOR
+  TOPIC_LED_COLOR,
+  TOPIC_LED_ENABLED
 };
 const int NUM_TOPICS = sizeof(TOPICS) / sizeof(TOPICS[0]);
 #endif
