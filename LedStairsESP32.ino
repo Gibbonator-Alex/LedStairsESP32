496edcd78adcea504fc9be98fa54951b40308566
Gibbonator-Alex <alexander.fitzgibbon@yahoo.de>
2025-12-18 20:12:00 +0100

    Added multithreading for wifi and mqttclient

--- LedStairsESP32.ino
+++ LedStairsESP32.ino
@@ -18,9 +18,7 @@ struct color{
   int g;
   int b;
 };
-
 const int NUM_PIXELS = 182;
-const int CONNECT_TIMEOUT = 5000;
 const color LED_COLOR_OFF{0, 0, 0};
 
 // ========================
@@ -31,20 +29,43 @@ int pinStatePrevious = LOW;
 int LIGHT_INTENSITY_TO_ACTIVATE_LED = 10;
 int DELAY_BETWEEN_PIXEL_ACTIVATION = 5;
 int LED_ON_DURATION = 30000; // ms
-bool LED_ENABLED = true;
 int MOTION_VALIDATION_DELAY_MS = 800; // ms
-
+bool LED_ENABLED = true;
 color LED_COLOR_ON{184, 12, 207};
-
 Adafruit_NeoPixel ws2812b(NUM_PIXELS, PIN_WS2812B, NEO_GRB + NEO_KHZ800);
-
-WiFiClient espClient;
-PubSubClient client(espClient);
 
 // ========================
 // WiFi & MQTT
 // ========================
 // definition in secrets.h
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+enum class MqttCommandType {
+  SET_LED_COLOR,
+  SET_LIGHT_INTENSITY,
+  SET_DELAY_BETWEEN_PIXEL,
+  SET_LED_ON_DURATION,
+  SET_LED_ENABLED
+};
+
+struct MqttCommand {
+  MqttCommandType type;
+  union {
+    int intValue;
+    struct {
+      uint8_t r;
+      uint8_t g;
+      uint8_t b;
+    } color;
+  };
+};
+QueueHandle_t mqttCommandQueue;
+
+// ========================
+// TASKS
+// ========================
+TaskHandle_t wifiMqttClientTask;
 
 // ========================
 // PROTOTYPES
@@ -59,38 +80,38 @@ bool isValidInt(const char *str, int length);
 // SETUP
 // ========================
 void setup() {
+  pinMode(PIN_HCSR501, INPUT);
+  
   ws2812b.begin();
   ws2812b.setBrightness(80);
 
-  ensureWiFiConnected();
-
-  client.setServer(mqtt_broker, mqtt_port);
-  client.setCallback(mqttCallback);
-  ensureMqttConnected();
-
-  pinMode(PIN_HCSR501, INPUT);
+  mqttCommandQueue = xQueueCreate(100, sizeof(MqttCommand));
+  
+  xTaskCreatePinnedToCore(
+    connectWifiMqttClient,
+    "WifiMqttClientConnectorTask",
+    10000,
+    NULL,
+    0,
+    &wifiMqttClientTask,
+    0
+  );
 }
 
 // ========================
 // LOOP
 // ========================
 void loop() {
-  ensureWiFiConnected();
-  ensureMqttConnected();
-  
   ws2812b.clear();
   ws2812b.show();
 
-  client.loop();
+  processMqttCommands();
   
   if(LED_ENABLED && isMotionDetected() && checkLightIntensity()){
     led(0, NUM_PIXELS, LED_COLOR_ON);
 
-    // Keep LEDs on for LED_ON_DURATION, allowing MQTT callbacks to run
     unsigned long startTime = millis();
     while(millis() - startTime < LED_ON_DURATION) {
-      client.loop();
-
       if(isMotionDetected())
         startTime = millis();
     }
@@ -100,45 +121,93 @@ void loop() {
 }
 
 // ========================
-// FUNCTIONS
+// WIFI MQTTCLIENT TASK
 // ========================
-void ensureWiFiConnected(){
-  if(WiFi.status() != WL_CONNECTED){
-    WiFi.begin(ssid, password);
-    unsigned long startAttemptTime = millis();
-    while(WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < CONNECT_TIMEOUT){
-      ws2812b.fill(ws2812b.Color(255, 0, 0), 0, 10);
-      ws2812b.show();
-      delay(250);
-      ws2812b.clear();
-      ws2812b.show();
-      delay(250);
-    }
-  }
-}
-
-void ensureMqttConnected(){
-  if(!client.connected()){
-    String client_id = "esp32-client-" + String(WiFi.macAddress());
-    
-    unsigned long startAttemptTime = millis();
-    while(!client.connect(client_id.c_str()) && millis() - startAttemptTime < CONNECT_TIMEOUT){
-      ws2812b.fill(ws2812b.Color(255, 0, 0), 0, 10);
-      ws2812b.show();
-      delay(250);
-      ws2812b.clear();
-      ws2812b.show();
-      delay(250); 
+void connectWifiMqttClient( void *parameter )
+{
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(mqttCallback);
+  
+  for(;;) {
+    if( WiFi.status() != WL_CONNECTED)
+    {
+      WiFi.begin(ssid, password);
     }
 
-    if(client.connected()){
-      for(int i = 0; i < NUM_TOPICS; i++){
-        client.subscribe(TOPICS[i]);
+    if( !client.connected() )
+    {
+      String clientId = "esp32-ledStairs-client-" + String(WiFi.macAddress());
+      client.connect(clientId.c_str());
+
+      if(client.connected()){
+        for(int i = 0; i < NUM_TOPICS; i++){
+          client.subscribe(TOPICS[i]);
+        }
       }
     }
+    else
+    {
+      client.loop();
+    }
   }
+  vTaskDelete(NULL);
 }
 
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+  char msg[length + 1];
+  memcpy(msg, payload, length);
+  msg[length] = '\0';
+
+  MqttCommand cmd;
+
+  // ===== LED COLOR =====
+  if (strcmp(topic, TOPIC_LED_COLOR) == 0) {
+    int r, g, b;
+    if (sscanf(msg, "[%d,%d,%d]", &r, &g, &b) == 3) {
+      cmd.type = MqttCommandType::SET_LED_COLOR;
+      cmd.color.r = constrain(r, 0, 255);
+      cmd.color.g = constrain(g, 0, 255);
+      cmd.color.b = constrain(b, 0, 255);
+    } else {
+      return;
+    }
+  }
+  // ===== INT TOPICS =====
+  else {
+    if (!isValidInt(msg, length)) return;
+
+    int val = atoi(msg);
+    cmd.intValue = val;
+
+    if (strcmp(topic, TOPIC_LIGHT_INTENSITY) == 0)
+      cmd.type = MqttCommandType::SET_LIGHT_INTENSITY;
+    else if (strcmp(topic, TOPIC_DELAY_BETWEEN_PIX) == 0)
+      cmd.type = MqttCommandType::SET_DELAY_BETWEEN_PIXEL;
+    else if (strcmp(topic, TOPIC_LED_ON_DURATION) == 0)
+      cmd.type = MqttCommandType::SET_LED_ON_DURATION;
+    else if (strcmp(topic, TOPIC_LED_ENABLED) == 0)
+      cmd.type = MqttCommandType::SET_LED_ENABLED;
+    else
+      return;
+  }
+
+  xQueueSend(mqttCommandQueue, &cmd, 0);
+}
+
+
+bool isValidInt(const char *str, int length){
+  for(int i = 0; i < length; i++){
+    if(!isDigit(str[i])) return false;
+  }
+
+  return true;
+}
+
+
+// ========================
+// NORMAL TASK
+// ========================
 bool isMotionDetected() {
   static unsigned long motionStart = 0;
   static bool waitingForValidation = false;
@@ -180,52 +249,32 @@ void led(const int& fromPixel, const int& toPixel, const color c){
   }
 }
 
-void mqttCallback(char *topic, byte *payload, unsigned int length) {
-  char msg[length + 1];
-  memcpy(msg, payload, length);
-  msg[length] = '\0';
-  
-  // ======= LED Color Topic =======
-  if(strcmp(topic, TOPIC_LED_COLOR) == 0) {
-    int r, g, b;
-    if(sscanf(msg, "[%d,%d,%d]", &r, &g, &b) == 3){
-      r = constrain(r, 0, 255);
-      g = constrain(g, 0, 255);
-      b = constrain(b, 0, 255);
-      LED_COLOR_ON = {r, g, b};
-    }
-    else {
-      return;
-    }
-  }
-  // ======= Number-Topics =======
-  else {
-    if(!isValidInt(msg, length)) return;
-    int val = atoi(msg);
-    
-    if(strcmp(topic, TOPIC_LIGHT_INTENSITY) == 0) {
-      LIGHT_INTENSITY_TO_ACTIVATE_LED = val;
-    } 
-    else if(strcmp(topic, TOPIC_DELAY_BETWEEN_PIX) == 0) {
-      DELAY_BETWEEN_PIXEL_ACTIVATION = val;
-    } 
-    else if(strcmp(topic, TOPIC_LED_ON_DURATION) == 0) {
-      LED_ON_DURATION = val;
-    }
-    else if(strcmp(topic, TOPIC_LED_ENABLED) == 0) {
-      if(val == 1){
-        LED_ENABLED = true;
-      } else if(val == 0){
-        LED_ENABLED = false;
-      }
-    }
-  }
-}
+void processMqttCommands()
+{
+  MqttCommand cmd;
+  if (xQueueReceive(mqttCommandQueue, &cmd, 0))
+  {
+    switch (cmd.type) {
 
-bool isValidInt(const char *str, int length){
-  for(int i = 0; i < length; i++){
-    if(!isDigit(str[i])) return false;
-  }
+      case MqttCommandType::SET_LED_COLOR:
+        LED_COLOR_ON = {cmd.color.r, cmd.color.g, cmd.color.b};
+        break;
 
-  return true;
+      case MqttCommandType::SET_LIGHT_INTENSITY:
+        LIGHT_INTENSITY_TO_ACTIVATE_LED = cmd.intValue;
+        break;
+
+      case MqttCommandType::SET_DELAY_BETWEEN_PIXEL:
+        DELAY_BETWEEN_PIXEL_ACTIVATION = cmd.intValue;
+        break;
+
+      case MqttCommandType::SET_LED_ON_DURATION:
+        LED_ON_DURATION = cmd.intValue;
+        break;
+
+      case MqttCommandType::SET_LED_ENABLED:
+        cmd.intValue == 0 ? LED_ENABLED = false : LED_ENABLED = true;
+        break;
+    }
+  }
 }
